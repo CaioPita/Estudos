72d4e92a43b0cebc80e22997cdf51291419be92f
CaioPita <caio-gama@outlook.com>
2025-04-14 17:50:14 -0300

    Add files via upload

---
+++ Padroes de projeto em Java/Aula05/src/Manager/Documento.java
@@ -0,0 +1,23 @@
+package Manager;
+
+import pessoa.Endereco;
+import pessoa.Pessoa;
+
+public class Documento {
+	private String titulo;
+	private String conteudo;
+	private String formato;
+	
+	public Documento(String titulo, String conteudo, String formato) {
+		this.titulo = titulo;
+		this.conteudo = conteudo;
+		this.formato = formato;
+	}
+	
+	public Documento clone() {
+		return new Documento(this.titulo, this.conteudo,this.formato);
+	}
+	
+	
+
+}
